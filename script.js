010d1b55280dc3fd45809847c95379ba4b437f7a
saurabh9303 <kumarsaurabh09546@gmail.com>
2026-05-08 09:03:42 +0530

    constructor function

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let obj = {
-    name : "saurabh"
+// let obj = {
+//     name : "saurabh"
+// }
+// const abc = function(a,b,c){
+//     console.log(this["name"],a,b,c)
+// }
+// abc.apply(obj, [1,2,3])
+
+
+function Factory (name, company, quantity, price){
+    this.name = name ;
+    this.company = company;
+    this.quantity = quantity;
+    this.price = price;
+    
 }
-const abc = function(a,b,c){
-    console.log(this["name"],a,b,c)
-}
-abc.apply(obj, [1,2,3])
+
+let Biskit1 = new Factory('ParleG', 'Parle', 5 , 10)
+console.log(Biskit1)
